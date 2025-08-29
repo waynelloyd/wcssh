d9ed848087fedde5acbd7f42668ff9edef3fdc0a
Wayne Lloyd <wayne.lloyd2@icloud.com>
2025-08-29 11:26:36 +0100

    Fixed sha for homebrew repo

--- Formula/wcssh.rb
+++ Formula/wcssh.rb
@@ -2,7 +2,7 @@ class Wcssh < Formula
   desc "A Warp-native multi-SSH launcher that opens a new window, splits panes, and enables synchronized input by default."
   homepage "https://github.com/waynelloyd/homebrew-wcssh"
   url "https://github.com/waynelloyd/homebrew-wcssh/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed" # Replace this with the output of: shasum -a 256 v0.1.0.tar.gz
+  sha256 "ca6c921e028505058d80f742f0856a7d8b687896451f44485f72daedb8c16001" # Replace this with the output of: shasum -a 256 v0.1.0.tar.gz
   license "MIT"
 
   def install
