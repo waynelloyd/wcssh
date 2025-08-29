624b2dcac05761a5260f2d886a2ba9284fc49cde
Wayne Lloyd <wayne.lloyd2@icloud.com>
2025-08-29 12:32:25 +0100

    Updated hash

--- Formula/wcssh.rb
+++ Formula/wcssh.rb
@@ -2,7 +2,7 @@ class Wcssh < Formula
   desc "A Warp-native multi-SSH launcher that opens a new window, splits panes, and enables synchronized input by default."
   homepage "https://github.com/waynelloyd/homebrew-wcssh"
   url "https://github.com/waynelloyd/homebrew-wcssh/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8c6ee10c56287d597095781ce770b5e8df3f6c36f2affe28b283db3bd423a805" # Replace this with the output of: shasum -a 256 v0.1.0.tar.gz
+  sha256 "83e15b8198d0f3e5ba4c1a99e1c17f258c5dfebdbf39341e058a9392addaaa8a" # Replace this with the output of: shasum -a 256 v0.1.0.tar.gz
   license "MIT"
 
   def install
