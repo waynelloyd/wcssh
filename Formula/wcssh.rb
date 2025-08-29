66fd423e96445639e61fcd10471235699097f5eb
Wayne Lloyd <wayne.lloyd2@icloud.com>
2025-08-29 17:49:32 +0100

    Bumped release version to 1.0.1

--- Formula/wcssh.rb
+++ Formula/wcssh.rb
@@ -1,7 +1,7 @@
 class Wcssh < Formula
   desc "A Warp-native multi-SSH launcher that opens a new window, splits panes, and enables synchronized input by default."
   homepage "https://github.com/waynelloyd/homebrew-wcssh"
-  url "https://github.com/waynelloyd/homebrew-wcssh/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/waynelloyd/homebrew-wcssh/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "57ccb7e72bab287bb70fb7d02337fe9c939031e6d94a9ae4e325ee0ebeda2dd2" # The checksum of the new v1.0.0 tarball
   license "MIT"
 
